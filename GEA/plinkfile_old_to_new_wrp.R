6e3769c6a74fa00993221851ca700cdc14188a53
nminad <58418429+nminad@users.noreply.github.com>
2023-08-18 11:07:48 +0200

    Create plinkfile_old_to_new_wrp.R

---
+++ GEA/plinkfile_old_to_new_wrp.R
@@ -0,0 +1,22 @@
+# Put ONLY the chromosome number in the first column of the .map file
+
+library(tidyverse)
+
+setwd("/media/nikos/LaCie/Nikos_data/Lineage_Clade_vcf/noHe_Analysis//0_data")
+
+# File name to rename
+filename <- 'plinkfile.map'
+
+# Check if file exists.
+if (file.exists(filename)) {
+  # Rename file name
+  file.rename(filename,'plinkfile_old.map')
+}else{
+  print('File Not found :')
+}
+
+old_map <- read.delim("plinkfile_old.map", sep = "", header = F)
+
+old_map$V1 <- substring(old_map$V2, 3, 3)
+
+write.table(old_map, 'plinkfile.map', sep = " ", row.names = F, col.names = F, quote = F)
