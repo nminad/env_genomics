9237f53dc2cc1ad17e7676fc0d9af93e371a5709
nminad <58418429+nminad@users.noreply.github.com>
2023-08-18 11:15:51 +0200

    Update gemma_loop_wrp.sh

--- GEA/gemma_loop_wrp.sh
+++ GEA/gemma_loop_wrp.sh
@@ -3,7 +3,7 @@
 while read line; do
 	NUM=$(echo $line | cut -d',' -f2)
 	CODENAME=$(echo $line | cut -d',' -f3)
-	/home/nikos/software/gemma-0.98.5-linux-static-AMD64 -bfile ../0_data/Bdis332_SNPs_Yann_final_noHet_binary \
+	/home/nikos/software/gemma-0.98.5-linux-static-AMD64 -bfile ../0_data/binary \
 	-k ../0_data/output/structure.cXX.txt -lmm 4 -n $NUM -maf 0.05 -o ULMM_${CODENAME}_output
 done < phenotype_names.csv
 
