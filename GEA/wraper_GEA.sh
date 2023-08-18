051778662bb217b1f9cec8fd5e66a04319d3d151
nminad <58418429+nminad@users.noreply.github.com>
2023-08-18 10:33:19 +0200

    Create wraper_GEA.sh

---
+++ GEA/wraper_GEA.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+vcftools --gzvcf ../0_data/Yann_filt_no_missing_noHet.vcf.gz --plink --out ../0_data/plinkfile
+
+Rscript plinkfile_old_to_new.R
+
+plink1.9 --file ../0_data/plinkfile --make-bed --out ../0_data/binary --noweb
+
+Rscript binary_fam_old_to_new.R
+
+bash gemma_loop.sh
+
+mkdir ../2_output/gemma_output
+
+mv ./output/* ../2_output/gemma_output/
+
+#FDR
+Rscript regions_of_significance_gemma_loop_FDR_wrp.R
+
+bash ./bedtools_intersect_loop_FDR_wrp.sh
+
+Rscript gene_lists_gene_search_FDR_wrp.R
+
+Rscript Loop_for_plots_GWAS_wrp.R
